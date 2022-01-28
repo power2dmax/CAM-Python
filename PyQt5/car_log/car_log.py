39a21a00c699ace7c489ea2a84e6d11f15f71ae3
power2dmax <98193974+power2dmax@users.noreply.github.com>
2022-01-27 20:45:58 -0500

    Update

--- PyQt5/car_log/car_log.py
+++ PyQt5/car_log/car_log.py
@@ -1,17 +1,20 @@
 # car_log.py
 """
 Program written in Python is used to track vehicle maintenance.
-This program uses PyQt5 for the UI and SQLite.
+This program uses PyQt5 for the UI and csv as a flat file.
 The user can enter the date, millage, and the maintence performed.
 The user will also be able to delete a row
 """
 
 import sys, csv
+import pandas as pd
 
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui as qtg
 from PyQt5 import QtCore as qtc
 from PyQt5 import QtSql as qts
+
+
 
 class MainWindow(qtw.QMainWindow):
     """
@@ -21,28 +24,10 @@ class MainWindow(qtw.QMainWindow):
         super().__init__()
         
         self.setWindowTitle("My Car Log App")
-        self.setMinimumSize(qtc.QSize(500, 600))
+        self.setMinimumSize(qtc.QSize(550, 675))
 
-        #self.createConnection()
         self.createMenu()
-
         self.car_log_tracker()
-        
-    def createConnection(self):
-        database = qts.QSqlDatabase.addDatabase("QSQLITE")
-        database.setDatabaseName("files/car_log.db")
-    
-        if not database.open():
-            print ("Unable to open data source file")
-            sys.exit(1) # Error code 1 - signifies error
-            
-        # Check if the tables we need exist in the database
-        tables_needed = {'maintenance'}
-        tables_not_found = tables_needed - set(database.tables())
-        if tables_not_found:
-            qtw.QMessageBox.critical(None, 'Error',
-                f'The following tables are missing from the database:{tables_not_found}')
-            sys.exit(1) # Error code 1 - signifies error
         
     def createMenu(self):
         # Create the actions for the "File" menu
@@ -56,16 +41,19 @@ class MainWindow(qtw.QMainWindow):
         self.exit_action.triggered.connect(self.close)
         
         # Create the actions for the "Edit Menu"
-        self.add_action = qtw.QAction('Add', self)
+        self.add_action = qtw.QAction('Add Row', self)
         self.add_action.setShortcut('Ctrl+A')
-        self.add_action.triggered.connect(self.add_entry)
+        self.add_action.setIcon(qtg.QIcon("icons/add_row.png"))
+        self.add_action.triggered.connect(self.addRow)
         
-        self.del_action = qtw.QAction('Delete', self)
+        self.del_action = qtw.QAction('Delete Row', self)
         self.del_action.setShortcut('Ctrl+D')
-        self.del_action.triggered.connect(self.delete_entry)
+        self.del_action.setIcon(qtg.QIcon("icons/del_row.png"))
+        self.del_action.triggered.connect(self.removeRow)
         
         # Create actions for the "Help Menu" menu
         about_action = qtw.QAction('About', self)
+        about_action.setIcon(qtg.QIcon("icons/about.png"))
         #about_action.triggered.connect(self.close)
         
         # Create the menubar
@@ -83,34 +71,37 @@ class MainWindow(qtw.QMainWindow):
         
         help_menu = menu_bar.addMenu('Help')
         help_menu.addAction(about_action)
-        
        
     def car_log_tracker(self):
         """
         Create instances of widgets, the table view and set layouts
         """
-        icons_path = "images/icons"
-        
         title = qtw.QLabel("Car Maintenance Log")
         title.setSizePolicy(qtw.QSizePolicy.Fixed, qtw.QSizePolicy.Fixed)
         title.setStyleSheet("font: bold 24px")
         
-        add_record_button = qtw.QPushButton("Add Maintenance")
-        #add_record_button.setIcon(qtw.QIcon(os.path.join(icons_path, "add_user.png")))
+        add_record_button = qtw.QPushButton("Add Row")
+        add_record_button.setIcon(qtg.QIcon("icons/add_row.png"))
         add_record_button.setStyleSheet("padding: 10px")
-        #add_record_button.clicked.connect(self.addRecord)
+        add_record_button.clicked.connect(self.addRow)
         
-        del_record_button = qtw.QPushButton("Delete")
-        #del_record_button.setIcon(qtw.QIcon(os.path.join(icons_path, "del_user.png")))
+        del_record_button = qtw.QPushButton("Delete Row")
+        del_record_button.setIcon(qtg.QIcon("icons/del_row.png"))
         del_record_button.setStyleSheet("padding: 10px")
-        #del_record_button.clicked.connect(self.deleteRecord)
+        del_record_button.clicked.connect(self.removeRow)
+        
+        save_button = qtw.QPushButton("Save")
+        #del_record_button.setIcon(qtw.QIcon(os.path.join(icons_path, "del_user.png")))
+        save_button.setStyleSheet("padding: 10px")
+        save_button.clicked.connect(self.save_file)
         
         exit_button = qtw.QPushButton('Exit', self)
         exit_button.setStyleSheet("padding: 10px")
         exit_button.clicked.connect(self.close) 
         
         # Set up the sorting combo box
-        sorting_options = ["Date", "Mileage", "Action"]
+        sorting_text = qtw.QLabel('Sorting Options: ')
+        sorting_options = ["Date", "Mileage", "Maintenance"]
         sort_name_cb = qtw.QComboBox()
         sort_name_cb.addItems(sorting_options)
         #sort_name_cb.currentTextChanged.connect(self.setSortingOrder)
@@ -118,25 +109,30 @@ class MainWindow(qtw.QMainWindow):
         buttons_h_box = qtw.QHBoxLayout()
         buttons_h_box.addWidget(add_record_button)
         buttons_h_box.addWidget(del_record_button)
-        buttons_h_box.addWidget(exit_button)
-        buttons_h_box.addStretch()
+        buttons_h_box.addWidget(sorting_text)
         buttons_h_box.addWidget(sort_name_cb)
+        buttons_h_box.addStretch()
+        buttons_h_box.addWidget(save_button)
+        buttons_h_box.addWidget(exit_button)
+        
         
         # Widget to contain editing buttons
         edit_buttons = qtw.QWidget()
         edit_buttons.setLayout(buttons_h_box)
         
+        # Time to set up the table to display
         self.model = qtg.QStandardItemModel()
-        
         self.table_view = qtw.QTableView()
-        self.table_view.SelectionMode(3)
+        
+        self.header = self.table_view.horizontalHeader()
+        self.header.setStretchLastSection(True)
         self.table_view.setModel(self.model)
         
-        self.model.setRowCount(12)
-        self.model.setColumnCount(2)
+        #self.model.setRowCount(9)
+        #self.model.setColumnCount(2)
         
+        # Load the cvs file up for display and editing
         self.loadCSVFile()
-        
                
         # Main Layout
         main_v_box = qtw.QVBoxLayout()
@@ -147,11 +143,16 @@ class MainWindow(qtw.QMainWindow):
         widget.setLayout(main_v_box)
         self.setCentralWidget(widget)
         
-        
-    def add_entry(self):
-        pass
+    def addRow(self):
+       item = qtg.QStandardItem("")
+       self.model.appendRow(item)
     
-    def delete_entry(self):
+    def removeRow(self):
+       indices = self.table_view.selectionModel().selectedRows() 
+       for index in sorted(indices):
+           model.removeRow(index.row())
+           
+    def save_file(self):
         pass
     
     def loadCSVFile(self):
@@ -168,9 +169,7 @@ class MainWindow(qtw.QMainWindow):
             for i, row in enumerate(csv.reader(csv_f)):
                 items = [qtg.QStandardItem(item) for item in row]
                 self.model.insertRow(i, items)
-    
-    
-        
+  
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
     mainWin = MainWindow()
